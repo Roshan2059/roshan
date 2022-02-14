8d8e94453633c00826e9b12916b1eb3c84a34e19
unknown <pradippandey455@gmail.com>
2022-02-14 22:07:37 +0545

    jstophp conversion
    converted js inserted components into php code

--- index.php
+++ index.php
@@ -14,8 +14,75 @@
     include 'db_queries.php';
     ?>
     
+        function debug($arre){
+            echo "<pre>";
+            print_r($arre);
+            echo "</pre>";
+        }
+
+        function letterProvider($n)
+        {
+            // letters = 'abcdefghijklmnopqrstuvwxyz';
+            // each = letters.split('');
+            // len = each.length;
+            // if(n<len && n>=0)
+            // {
+            //     return each[n].toUpperCase();
+            // }
+            // return 0;
+
+            $first = 'A';
+            for($i = 0; $i < 26; $i++)
+            {
+                if($n == $i)
+                {
+                    return $first;
+                }
+                $first++;
+            }
+            return 0;
+        }
+
+        function create_data($size)
+        {
+            $seats = array();
+
+            function statusProvider()
+            {
+                $state = ["available","reserved","booked"];
+                $val = rand(0,2);
+                return $state[$val];
+            }
+
+            for($i=0;$i<$size;$i++)
+            {
+                $col = $i%15;
+                $row = floor($i/15);
+                
+                array_push($seats,[
+                    "seat_id"=>letterProvider($row).($col+1),
+                    "status"=>statusProvider()
+                ]);
+            }
+            return $seats;
+            
+        }
+        $dispaly_data = create_data(120);
+        //  echo letterProvider(-5);
+        // create_data(10);
+
+
+        
+        $statusMap = [
+            "available"=>"seats available",
+            "reserved"=>"seats reserved",
+            "booked"=>"seats booked",
+            "selected"=>"seats booked"
+        ];
+
+    ?>
     <div class="first-container">
-            <span class="big">MUSK  Theater</span>
+            <span class="big">MUSK Theater</span>
              <span class="small">online ticket reservation</span>   
     </div>
     
@@ -25,15 +92,69 @@
 
         </div>
 
-        <div class="box" id="first-box">
+        <div class="box" id="zero-box">
+            <?php
+                forEach($dispaly_data as $index => $indi)
+                {
+                    
+                    $bar = floor(($index%15));
+                    if($bar==0)
+                    {
+                        // echo "<div class='".$statusMap[$indi['status']]."' data-status='".$indi['status']."'>"
+                        // .$indi['seat_id']
+                        // ."</div>";
 
+                        echo "<div class='seats' id='seatlabel'>".letterProvider(floor($index/15))."</div>";
+                    }
+                }
+            ?>
         </div>
 
         <div class="box" id="second-box">
+            <?php
+                forEach($dispaly_data as $index => $indi)
+                {
+                    
+                    $bar = floor(($index%15)/5);
+                    if($bar==0)
+                    {
+                        echo "<div class='".$statusMap[$indi['status']]."' data-status='".$indi['status']."'>"
+                        .$indi['seat_id']
+                        ."</div>";
+                    }
+                }
+            ?>
+        </div>
 
+        <div class="box" id="second-box">
+            <?php
+                forEach($dispaly_data as $index => $indi)
+                {
+                    
+                    $bar = floor(($index%15)/5);
+                    if($bar==1)
+                    {
+                        echo "<div class='".$statusMap[$indi['status']]."' data-status='".$indi['status']."'>"
+                        .$indi['seat_id']
+                        ."</div>";
+                    }
+                }
+            ?>
         </div>
 
         <div class="box" id="third-box">
+            <?php
+                forEach($dispaly_data as $index => $indi)
+                {
+                    $bar = floor(($index%15)/5);
+                    if($bar==2)
+                    {
+                        echo "<div class='".$statusMap[$indi['status']]."' data-status='".$indi['status']."'>"
+                        .$indi['seat_id']
+                        ."</div>";
+                    }
+                }
+            ?>
             
         </div>
        
--- script.js
+++ script.js
@@ -54,79 +54,84 @@ window.addEventListener('DOMContentLoaded',()=>{
         "reserved":"#2EB086",
         "booked":"#f55142"
     }
-    function createSeat(item,index,arr)
-    {
-        const seat = document.createElement('div');
-        seat.className = 'seats';
-        seat.innerText = item['seat_id'];
-        seat.style.background = colormap[item['status']];
-        seat.part.value = item['status'];
-        if(item['status']=='available')
-        {
-            seat.style.cursor = 'pointer';
-            seat.addEventListener('mouseover',()=>
-            {
-                if(item['status']=='available')
-                seat.style.color = '#313552';
-            });
-            seat.addEventListener('mouseout',()=>
-            {
-                seat.style.color = 'transparent';
-            });
-        }
-        else{
-            seat.style.cursor = 'default';
-        }
+    // function createSeat(item,index,arr)
+    // {
+    //     const seat = document.createElement('div');
+    //     seat.className = 'seats';
+    //     seat.innerText = item['seat_id'];
+    //     seat.style.background = colormap[item['status']];
+    //     seat.part.value = item['status'];
+    //     if(item['status']=='available')
+    //     {
+    //         seat.style.cursor = 'pointer';
+    //         seat.addEventListener('mouseover',()=>
+    //         {
+    //             if(item['status']=='available')
+    //             seat.style.color = '#313552';
+    //         });
+    //         seat.addEventListener('mouseout',()=>
+    //         {
+    //             seat.style.color = 'transparent';
+    //         });
+    //     }
+    //     else{
+    //         seat.style.cursor = 'default';
+    //     }
 
-        //Adding the letter index for each new column
-        if (index%15 == 0){
-            const ind = document.createElement('div');
-            ind.className = 'seats';
-            ind.style.background = 'none';
-            ind.innerText = letterProvider(Math.floor(index/15));
-            ind.style.color = '#313552';
-            ind.style.border = '2px solid transparent';
-            ind.style.padding = '4px';
-            zeroBox.appendChild(ind);
-        }
+    //     //Adding the letter index for each new column
+    //     if (index%15 == 0){
+    //         const ind = document.createElement('div');
+    //         ind.className = 'seats';
+    //         ind.style.background = 'none';
+    //         ind.innerText = letterProvider(Math.floor(index/15));
+    //         ind.style.color = '#313552';
+    //         ind.style.border = '2px solid transparent';
+    //         ind.style.padding = '4px';
+    //         zeroBox.appendChild(ind);
+    //     }
 
 
-        let bar = Math.floor((index%15)/5); 
-        if(bar == 0)
-        {
-            firstBox.appendChild(seat);
-            console.log()
-        }
-        else if(bar == 1)
-        {
-            secondBox.appendChild(seat);
-        }
-        else if(bar == 2)
-        {
-            thirdBox.appendChild(seat);
-        }
-    }
-    todayData.forEach(createSeat);
+    //     let bar = Math.floor((index%15)/5); 
+    //     if(bar == 0)
+    //     {
+    //         firstBox.appendChild(seat);
+    //         console.log()
+    //     }
+    //     else if(bar == 1)
+    //     {
+    //         secondBox.appendChild(seat);
+    //     }
+    //     else if(bar == 2)
+    //     {
+    //         thirdBox.appendChild(seat);
+    //     }
+    // }
+    // todayData.forEach(createSeat);
 });
 
 const seats = document.getElementsByClassName('seats');
 
 container.addEventListener('click',(event)=>{
-    // console.log(event);
-    const isSeat = event.target.className == 'seats';
-    const value = event.target.part.value;
+    console.log(event);
+    const isSeat = Object.keys(event.target.classList).some((k)=>(event.target.classList[k]==='seats'));
+    console.log(event.target.classList);
+    console.log(isSeat);
+    const value = event.target.dataset.status;
     if(isSeat && !(value=='reserved' || value=='booked'))
     {
         seatText = event.target.innerText;
         if(!selectedSeats.includes(seatText))
         {
-            event.target.style.background = '#313552';
+            // event.target.style.background = '#313552';
+            event.target.className = 'seats selected';
             selectedSeats.push(seatText);
             console.log(selectedSeats);
         }
         else
         {
-            event.target.style.background = 'none';
+            
+            event.target.className = 'seats available';
+            // event.target.style.background = 'none';
             selectedSeats = selectedSeats.filter(item=>item!==seatText);
             console.log(selectedSeats);
         }
