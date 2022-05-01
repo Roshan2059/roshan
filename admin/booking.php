5b1e178b9f7c22328d1fa0c08bad2d0a90b3caf9
Roshan <roshanpanta2059@gmail.com>
2022-05-01 17:01:16 +0545

    added booking.css and dashboard.css

--- admin/booking.php
+++ admin/booking.php
@@ -8,49 +8,7 @@
     <title>Document</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            --primary: #5e00c9;
-            --primaryA: #313552;
-        }
-
-        body {
-            background-color: whitesmoke;
-            font-family: 'Montserrat', sans-serif;
-            font-weight: bold;
-        }
-        table {
-            position: absolute;
-            top: 10vh;
-            left: 15vw;
-            border: none;
-            width: 80vw;
-            height: auto;
-            border-collapse: separate;
-            border-spacing: 0 0.8em;
-        }
-
-        th {
-            padding: 30px;
-            margin: 10px;
-            font-weight: bolder;
-        }
-
-        tr {
-            text-align: center;
-            background-color: pink;
-            color: blue;
-            font-size: 20px;
-            padding: 10px;
-            box-sizing: border-box;
-        }
-
-        td {
-            padding: 20px;
-        }
-    </style>
+    <link rel="stylesheet" href="booking.css">
 </head>
 
 <body>
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -8,57 +8,8 @@
     <title>RJ Movies Admin Panel</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dashboard.css">
 </head>
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-        --primary: #5e00c9;
-        --primaryA: #313552;
-    }
-
-    body {
-        background-color: whitesmoke;
-        font-family: 'Montserrat', sans-serif;
-        font-weight: bold;
-    }
-
-    .main {
-        width: 85vw;
-        height: auto;
-        display: flex;
-        flex-wrap: wrap;
-        /* padding-top: 3vh; */
-        position: relative;
-        left: 12vw;
-        top: 10vh;
-    }
-
-    .box {
-        background-color: pink;
-        height: 35vh;
-        width: 22vw;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-        box-sizing: border-box;
-        margin: 30px 40px;
-        box-shadow: 5px 5px 15px gray;
-        color: blue;
-        padding: 1em;
-    }
-
-    .box h1 {
-        display: flex;
-        flex-wrap: wrap;
-        font-size: 1.5em;
-        width: max-content;
-        height: 5vh;
-        margin-left: auto;
-        margin-right: auto;
-        font-weight: 1000;
-    }
-</style>
 
 <body>
     <?php include '../partials/navnside.php'; ?>
