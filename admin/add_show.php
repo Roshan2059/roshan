5fbcfc5a6e72a40bfb17321555b7810ffa69008e
Roshan <roshanpanta2059@gmail.com>
2022-05-01 16:56:08 +0545

    added add_show.css

--- admin/add_show.php
+++ admin/add_show.php
@@ -7,48 +7,7 @@
     <title>Document</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-        }
-        form{  
-            width: 30vw;
-            height: 90vh;
-            padding: 20px;
-            margin-left: auto;
-            margin-right: auto;
-        }
-        h1{
-            font-family: 'Montserrat', sans-serif;
-            margin-bottom: 5vh;
-        }
-        label{
-            margin-top: 5vh;
-            font-size: 20px;
-            font-family: 'Montserrat', sans-serif;
-            font-weight: bold;
-        }
-        input{
-            padding: 0.5em;
-            font-size: 20px;
-            font-family: 'Montserrat', sans-serif;
-            font-weight: bold;
-            margin-bottom: 5vh;
-        }
-        a{
-            text-decoration: none;
-            padding: 8px;
-            background-color: blue;
-            color: white;
-            border-radius: 5px;
-            text-align: center;
-        }
-        a:hover{
-            color: white;
-            background-color: springgreen;
-        }
-    </style>
+    <link rel="stylesheet" href="add_show.css">
 </head>
 <body>
 <!-- <?php include '../partials/navnside.php'; ?> -->
